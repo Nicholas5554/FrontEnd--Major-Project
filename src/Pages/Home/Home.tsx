d4fd1a186a066159baed68908b38d1d21ee941b8
ghost5554 <nick555310@gmail.com>
2025-04-26 13:20:45 +0300

    added more ui change to phone screens

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import 'animate.css';
+import { IoIosPhoneLandscape } from "react-icons/io";
+
 
 
 const Home = () => {
@@ -15,6 +17,7 @@ const Home = () => {
                 <h1 className="text-7xl animate__animated animate__fadeIn">Time To Move NEX</h1>
                 <p className="text-4xl animate__animated animate__fadeIn">Minimalist task organizer</p>
                 <button type="button" className="text-xl text-white transition-all duration-300 ease-in-out bg-blue-600 rounded-md h-11 animate__animated animate__fadeIn w-60 hover:bg-blue-700 hover:scale-105 hover:shadow-lg" onClick={navToRegister}>Take me to the NEX Level</button>
+                <IoIosPhoneLandscape className="size-28 md:size-0 lg:size-0 animate__animated animate__swing animate__slow"></IoIosPhoneLandscape>
             </div>
         </>
     )
