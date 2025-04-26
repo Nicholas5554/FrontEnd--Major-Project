c4e6a2d1f9a2afad95e43ff465febba33a1f8eb7
ghost5554 <nick555310@gmail.com>
2025-04-26 16:14:03 +0300

    added phone flip ui

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
                 <h1 className="text-7xl animate__animated animate__fadeIn">Time To Move NEX</h1>
                 <p className="text-4xl animate__animated animate__fadeIn">Minimalist task organizer</p>
                 <button type="button" className="text-xl text-white transition-all duration-300 ease-in-out bg-blue-600 rounded-md h-11 animate__animated animate__fadeIn w-60 hover:bg-blue-700 hover:scale-105 hover:shadow-lg" onClick={navToRegister}>Take me to the NEX Level</button>
-                <IoIosPhoneLandscape className="size-28 md:size-0 lg:size-0 animate__animated animate__swing animate__slow"></IoIosPhoneLandscape>
+                <IoIosPhoneLandscape className="size-28 md:size-0 lg:size-0 animate__animated animate__rotateInDownRight animate__slower animate__infinite animate__delay-1s"></IoIosPhoneLandscape>
             </div>
         </>
     )
