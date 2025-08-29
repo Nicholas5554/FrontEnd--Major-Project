d584da2afe82847ba7068ad3a2165cfe35ca9aba
ghost5554 <nick555310@gmail.com>
2025-08-29 16:27:32 +0300

    updated task details

--- src/Pages/TaskDetails/TaskDetails.tsx
+++ src/Pages/TaskDetails/TaskDetails.tsx
@@ -7,7 +7,6 @@ const TaskDetails = () => {
     return (
         <Card className="flex items-center justify-center w-auto text-center dark:text-white">
             <h1>Title : {task && task?.title!}</h1>
-            <h3>Type : {task && task?.type!}</h3>
             <p>the user who Created the Task : {task && task.userId && task.userId.name ? `${task.userId.name.first} ${task.userId.name.last}` : "Unknown"}</p>
             <p>the user who got it : {task && task.assignedTo ? `${task.assignedTo.name.first} ${task.assignedTo.name.last}` : "Unassigned"}</p>
             <p>Status : {task && task.status}</p>
